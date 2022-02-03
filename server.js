25564e2c044730ebbb65288bc8a20afd69a799ba
eriksvetlik <svetlik.erik@gmail.com>
2022-02-03 11:37:51 -0600

    added functions for adding department and role

--- server.js
+++ server.js
@@ -3,6 +3,39 @@ const inquirer = require("inquirer");
 const mysql2 = require("mysql2");
 require("dotenv").config();
 
+const addDepartment = [
+  {
+    type: "input",
+    message: "What is the department's name?",
+    name: "departmentName",
+  },
+];
+
+const addRole = [
+  {
+    type: "input",
+    message: "What is the roles's name?",
+    name: "roleName",
+  },
+  {
+    type: "input",
+    message: "What is the roles's salary?",
+    name: "roleSalary",
+  },
+  {
+    type: "list",
+    message: "Whice department does the role belong to?",
+    choices: [
+      "Accounting",
+      "Sales",
+      "Programming",
+      "Human Resources",
+      "Customer Support",
+    ],
+    name: "roleDepartment",
+  },
+];
+
 const initialQuestion = [
   {
     type: "list",
@@ -17,7 +50,7 @@ const initialQuestion = [
       "add department",
       "quit",
     ],
-    name: "initialQuestion",
+    name: "initialQuestionAnswer",
   },
 ];
 
@@ -34,43 +67,73 @@ const db = mysql2.createConnection(
 
 const prompt = () => {
   inquirer.prompt(initialQuestion).then((response) => {
-    if (response.initialQuestion === "view all employees") {
+    if (response.initialQuestionAnswer === "view all employees") {
+      const sql = "SELECT * FROM employees";
       // Query employees database
-      db.query("SELECT * FROM employees", function (err, results) {
+      db.query(sql, function (err, results) {
         console.table(results);
         prompt();
       });
     }
 
-    if (response.initialQuestion === "add employee") {
+    if (response.initialQuestionAnswer === "add employee") {
     }
 
-    if (response.initialQuestion === "update employee role") {
+    if (response.initialQuestionAnswer === "update employee role") {
     }
 
-    if (response.initialQuestion === "view all roles") {
+    if (response.initialQuestionAnswer === "view all roles") {
+      const sql = "SELECT * FROM roles";
       // Query roles database
-      db.query("SELECT * FROM roles", function (err, results) {
+      db.query(sql, function (err, results) {
         console.table(results);
         prompt();
       });
     }
 
-    if (response.initialQuestion === "add role") {
+    if (response.initialQuestionAnswer === "add role") {
+      inquirer.prompt(addRole).then((response) => {
+        const sql = `INSERT INTO roles (title, salary, department_id) 
+                    SELECT ?, ?, id
+                    FROM departments
+                    WHERE department_name = ?`;
+        const roleName = response.roleName;
+        const roleSalary = response.roleSalary;
+        const roleDepartment = response.roleDepartment;
+        // Adds role to the database
+        db.query(
+          sql,
+          [roleName, roleSalary, roleDepartment],
+          function (err, results) {
+            console.log(`${roleName} role added!`);
+            prompt();
+          }
+        );
+      });
     }
 
-    if (response.initialQuestion === "view all departments") {
+    if (response.initialQuestionAnswer === "view all departments") {
+      const sql = "SELECT * FROM departments";
       // Query departments database
-      db.query("SELECT * FROM departments", function (err, results) {
+      db.query(sql, function (err, results) {
         console.table(results);
         prompt();
       });
     }
 
-    if (response.initialQuestion === "add department") {
+    if (response.initialQuestionAnswer === "add department") {
+      inquirer.prompt(addDepartment).then((response) => {
+        const sql = `INSERT INTO departments (department_name) VALUES (?)`;
+        const departmentName = response.departmentName;
+        // Adds department to the database
+        db.query(sql, departmentName, function (err, results) {
+          console.log(`${departmentName} department added!`);
+          prompt();
+        });
+      });
     }
 
-    if (response.initialQuestion === "quit") {
+    if (response.initialQuestionAnswer === "quit") {
       console.log("goodbye!");
       process.exit(0);
     }
