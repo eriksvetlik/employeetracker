a6b16fe108b50c7ddc5393aace0760eb2cc98880
eriksvetlik <svetlik.erik@gmail.com>
2022-02-04 09:45:47 -0600

    added functions to add/update employee

--- server.js
+++ server.js
@@ -3,6 +3,36 @@ const inquirer = require("inquirer");
 const mysql2 = require("mysql2");
 require("dotenv").config();
 
+const addEmployee = [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "EmployeeFirstName",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "EmployeeLastName",
+  },
+  {
+    type: "list",
+    message: "What is the employee's role?",
+    choices: [
+      "Accountant",
+      "Salesperson",
+      "Programmer",
+      "HR Rep",
+      "Support Specialist",
+    ],
+    name: "EmployeeRole",
+  },
+  {
+    type: "input",
+    message: "Who is the employee's manager?",
+    name: "EmployeeManager",
+  },
+];
+
 const addDepartment = [
   {
     type: "input",
@@ -24,7 +54,7 @@ const addRole = [
   },
   {
     type: "list",
-    message: "Whice department does the role belong to?",
+    message: "Which department does the role belong to?",
     choices: [
       "Accounting",
       "Sales",
@@ -54,6 +84,38 @@ const initialQuestion = [
   },
 ];
 
+const updateEmployee = [
+  {
+    type: "list",
+    message: "Which employee's role would you like to update?",
+    choices: [
+      "Elliot",
+      "Amira",
+      "Christoper",
+      "Veronica",
+      "Igor",
+      "Evan",
+      "Evangeline",
+      "Safiyyah",
+      "Deen",
+      "Elspeth",
+    ],
+    name: "EmployeeName",
+  },
+  {
+    type: "list",
+    message: "What's their new role?",
+    choices: [
+      "Accountant",
+      "Salesperson",
+      "Programmer",
+      "HR Rep",
+      "Support Specialist",
+    ],
+    name: "EmployeeNewRole",
+  },
+];
+
 // Connect to database
 const db = mysql2.createConnection(
   {
@@ -68,7 +130,11 @@ const db = mysql2.createConnection(
 const prompt = () => {
   inquirer.prompt(initialQuestion).then((response) => {
     if (response.initialQuestionAnswer === "view all employees") {
-      const sql = "SELECT * FROM employees";
+      const sql = `SELECT e.id AS ID, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS Title, d.department_name AS Department, r.salary AS Salary, m.first_name AS Manager 
+                  FROM employees e 
+                  INNER JOIN roles r ON e.role_id = r.id 
+                  INNER JOIN departments d ON d.id = r.department_id 
+                  LEFT JOIN employees m ON e.manager_id = m.id;`;
       // Query employees database
       db.query(sql, function (err, results) {
         console.table(results);
@@ -77,13 +143,52 @@ const prompt = () => {
     }
 
     if (response.initialQuestionAnswer === "add employee") {
+      inquirer.prompt(addEmployee).then((response) => {
+        const sql = `INSERT INTO employees (first_name, last_name, role_id) 
+                    SELECT ?, ?, id
+                    FROM roles
+                    WHERE title = ?;`;
+        const EmployeeFirstName = response.EmployeeFirstName;
+        const EmployeeLastName = response.EmployeeLastName;
+        const EmployeeRole = response.EmployeeRole;
+        // Adds employee to the database
+        db.query(
+          sql,
+          [EmployeeFirstName, EmployeeLastName, EmployeeRole],
+          function (err, results) {
+            console.log(`${EmployeeFirstName} has been added!`);
+            prompt();
+          }
+        );
+      });
     }
 
     if (response.initialQuestionAnswer === "update employee role") {
+      inquirer.prompt(updateEmployee).then((response) => {
+        const sql = `UPDATE employees
+                    SET role_id = (
+                      SELECT id
+                      FROM roles
+                      WHERE title = ?
+                    )
+                    WHERE first_name = ?;`;
+        const EmployeeNewRole = response.EmployeeNewRole;
+        const EmployeeName = response.EmployeeName;
+        // Adds role to the database
+        db.query(sql, [EmployeeNewRole, EmployeeName], function (err, results) {
+          console.log(
+            `${EmployeeName}'s role was changed to ${EmployeeNewRole}!`
+          );
+          prompt();
+        });
+      });
     }
 
     if (response.initialQuestionAnswer === "view all roles") {
-      const sql = "SELECT * FROM roles";
+      const sql = `SELECT r.title AS Title, r.id AS ID, d.department_name AS Department, r.salary AS Salary
+                  FROM roles r
+                  INNER JOIN departments d
+                  ON r.id = d.id`;
       // Query roles database
       db.query(sql, function (err, results) {
         console.table(results);
@@ -113,7 +218,8 @@ const prompt = () => {
     }
 
     if (response.initialQuestionAnswer === "view all departments") {
-      const sql = "SELECT * FROM departments";
+      const sql =
+        "SELECT id AS ID, department_name AS Department FROM departments";
       // Query departments database
       db.query(sql, function (err, results) {
         console.table(results);
